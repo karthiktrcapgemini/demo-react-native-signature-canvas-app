fb08f979867a2d1b19b46b86b8bc28177129d8e3
Karthik T R <karthik.tr@intelex.com>
2023-07-10 07:42:30 -0400

    initial commit for signature canvas

---
+++ App.tsx
@@ -0,0 +1,42 @@
+import React, {useRef} from 'react';
+import SignatureScreen, {SignatureViewRef} from 'react-native-signature-canvas';
+
+interface Props {
+  text: string;
+  onOK: (signature: any) => void;
+}
+
+const App: React.FC<Props> = ({text, onOK}) => {
+  const ref = useRef<SignatureViewRef>(null);
+
+  const handleSignature = (signature: any) => {
+    console.log(signature);
+    onOK(signature);
+  };
+
+  const handleEmpty = () => {
+    console.log('Empty');
+  };
+
+  const handleClear = () => {
+    console.log('clear success!');
+  };
+
+  const handleEnd = () => {
+    ref.current?.readSignature();
+  };
+
+  return (
+    <SignatureScreen
+      ref={ref}
+      onEnd={handleEnd}
+      onOK={() => console.log("OK Signature")}
+      onEmpty={handleEmpty}
+      onClear={handleClear}
+      autoClear={true}
+      descriptionText={text}
+    />
+  );
+};
+
+export default App;
